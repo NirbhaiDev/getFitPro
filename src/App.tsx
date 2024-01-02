4efe6a740084d49159a2836e4589a0e914e039f6
TechnoHammer7 <nirbhai.tiwari@testingxperts.com>
2024-01-02 17:01:20 +0530

    Setup: All the file setup before the projects

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Navbar from '@/scenes/Navbar';
+
+function App() {
+  return <div>
+     <h1 className="app  bg-gray-20">
+      Hello world!
+      <Navbar />
+    </h1>
+  </div>;
+}
+
+export default App;
